f649bca21784f099a8ec515cc3a77d579ead8579
Juanka_Socla <Juanka_Socla@DESKTOP-CLISTON>
2016-08-07 19:38:56 -0500

    Add project files.

---
+++ ImagineDevPage/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImagineDevPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
